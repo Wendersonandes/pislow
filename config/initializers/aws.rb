045051bf7780ad90bea900eb48a136e56a1d5b47
sechix <sechix@gmail.com>
2017-04-24 21:15:55 +0200

    active aws3

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,31 +1,31 @@
-# if Rails.env.production?
-#   attachment_config = {
-#     s3_credentials: {
-#       access_key_id:     ENV['AWS_ACCESS_KEY_ID'],
-#       secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-#       bucket:            ENV['S3_BUCKET_NAME']
-#     },
+if Rails.env.production?
+  attachment_config = {
+    s3_credentials: {
+      access_key_id:     ENV['AKIAJTWOFCQPZS5WDWKQ'],
+      secret_access_key: ENV['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'],
+      bucket:            ENV['pislow']
+    },
 
-#     storage:        :s3,
-#     s3_headers:     { 'Cache-Control' => 'max-age=31557600' },
-#     s3_protocol:    'https',
-#     bucket:         ENV['S3_BUCKET_NAME'],
-#     url:            ':s3_domain_url',
+    storage:        :s3,
+    s3_headers:     { 'Cache-Control' => 'max-age=31557600' },
+    s3_protocol:    'https',
+    bucket:         ENV['pislow'],
+    url:            ':s3_domain_url',
 
-#     styles: {
-#       micro: '44x56>',
-#       mini: ' 117x150>',
-#       small: '245x313>',
-#       product: '512x654>',
-#       large: '1422x1855>' ,
-#     },
+    styles: {
+            micro: '44x56>',
+            mini: ' 117x150>',
+            small: '245x313>',
+            product: '512x654>',
+            large: '1422x1855>',
+    },
 
-#     path:           '/:class/:id/:style/:basename.:extension',
-#     default_url:    'noimage/:style.png',
-#     default_style:  'product'
-#   }
+    path:           '/:class/:id/:style/:basename.:extension',
+    default_url:    'noimage/:style.png',
+    default_style:  'product'
+  }
 
-#   attachment_config.each do |key, value|
-#     Spree::Image.attachment_definitions[:attachment][key.to_sym] = value
-#   end
-# end
+  attachment_config.each do |key, value|
+    Spree::Image.attachment_definitions[:attachment][key.to_sym] = value
+  end
+end
