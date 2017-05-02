1aa4137a26b688bafa15a40b11ac215e24e4ef3b
sechix <sechix@gmail.com>
2017-05-02 16:37:15 +0200

    aws s3 error

--- config/initializers/alchemy_s3.rb
+++ config/initializers/alchemy_s3.rb
@@ -1,46 +1,25 @@
 
 if Rails.env.production?
-    # Upload files to S3 support Heroku (no local file storage) and clustering.
-    # Used in all environments so the database can be migrated between them without breaking images.
-    require 'dragonfly/s3_data_store'
-
-    # Pictures
-    Dragonfly.app(:alchemy_pictures).configure do
-      plugin :imagemagick
-      datastore :s3,
-                bucket_name: ENV['AWS_PICTURES_DIRECTORY'],
-                access_key_id: ENV['S3_ACCESS_KEY_ID'],
-                secret_access_key: ENV['S3_SECRET_ACCESS_KEY'],
-                store_meta: false,
-                storage_headers: {
-                  'Cache-Control' => 'public, max-age=31536000',
-                  'x-amz-acl' => 'public-read'
-                }
-      url_format '/pictures/:job/:basename.:format'
-      url_path_prefix Alchemy::MountPoint.get
-    end
-
-    # Attachments
-    Dragonfly.app(:alchemy_attachments).configure do
-      datastore :s3,
-                bucket_name: ENV['AWS_ATTACHMENTS_DIRECTORY'],
-                access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-                secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-                store_meta: false,
-                storage_headers: {
-                  'Cache-Control' => 'public, max-age=31536000',
-                  'x-amz-acl' => 'public-read'
-                }
-    end
-
-    # Logger
-    Dragonfly.logger = Rails.logger
-
-    # Add model functionality
-    if defined?(ActiveRecord::Base)
-      ActiveRecord::Base.extend Dragonfly::Model
-      ActiveRecord::Base.extend Dragonfly::Model::Validations
-    end
 
 
+  require 'dragonfly/s3_data_store'
+ 
+  aws_defaults = {
+    access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+    secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+    region: ENV['AWS_REGION'],
+    storage_headers: {'x-amz-acl' => 'public-read'},
+    url_scheme: 'https'
+  }
+ 
+  Dragonfly.app(:alchemy_pictures).configure do
+    plugin :imagemagick
+    datastore :s3,
+      bucket_name: ENV['AWS_PICTURES_DIRECTORY'],
+  end
+ 
+  Dragonfly.app(:alchemy_attachments).configure do
+    datastore :s3,
+      bucket_name: ENV['AWS_ATTACHMENTS_DIRECTORY'],
+  end
 end
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,1 @@
-# Be sure to restart your server when you modify this file.
-
-Rails.application.config.session_store :cookie_store, key: '_Pislow_web_session'
+PislowWeb::Application.config.session_store :cookie_store, key: '_alchemy_s3_session'
