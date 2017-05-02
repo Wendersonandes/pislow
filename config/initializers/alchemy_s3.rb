65ed042400380a62393d93323db4fe4ebf603854
sechix <sechix@gmail.com>
2017-05-02 18:24:24 +0200

    aws s3 error

--- config/initializers/alchemy_s3.rb
+++ config/initializers/alchemy_s3.rb
@@ -1,30 +1,49 @@
 
 if Rails.env.production?
 
-  require 'dragonfly'
-  require 'dragonfly/s3_data_store'
- 
-  aws_defaults = {
-    access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-    secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-    region: ENV['AWS_REGION'],
-    storage_headers: {'x-amz-acl' => 'public-read'},
-    url_scheme: 'https'
-  }
- 
-  Dragonfly.app(:alchemy_pictures).configure do
-    verify_urls false
-    plugin :imagemagick
-    datastore :s3,
-      bucket_name: ENV['AWS_PICTURES_DIRECTORY']
-    
-  end
- 
-  Dragonfly.app(:alchemy_attachments).configure do
-    datastore :s3,
-      bucket_name: ENV['AWS_ATTACHMENTS_DIRECTORY']
-  end
+# Alchemy CMS Dragonfly configuration.
 
+# Upload files to S3 support Heroku (no local file storage) and clustering.
+# Used in all environments so the database can be migrated between them without breaking images.
+require 'dragonfly/s3_data_store'
+
+# Pictures
+Dragonfly.app(:alchemy_pictures).configure do
+  plugin :imagemagick
+  datastore :s3,
+            bucket_name: ENV['AWS_PICTURES_DIRECTORY'],
+            access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+            secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+            store_meta: false,
+            storage_headers: {
+              'Cache-Control' => 'public, max-age=31536000',
+              'x-amz-acl' => 'public-read'
+            }
+  url_format '/pictures/:job/:basename.:format'
+  url_path_prefix '/store'
+end
+
+# Attachments
+Dragonfly.app(:alchemy_attachments).configure do
+  datastore :s3,
+            bucket_name: ENV['AWS_ATTACHMENTS_DIRECTORY'],
+            access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+            secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+            store_meta: false,
+            storage_headers: {
+              'Cache-Control' => 'public, max-age=31536000',
+              'x-amz-acl' => 'public-read'
+            }
+end
+
+# Logger
+Dragonfly.logger = Rails.logger
+
+# Add model functionality
+if defined?(ActiveRecord::Base)
+  ActiveRecord::Base.extend Dragonfly::Model
+  ActiveRecord::Base.extend Dragonfly::Model::Validations
+end
 
 
 end
