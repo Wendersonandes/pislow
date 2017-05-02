888919a60837a2c7b41418b46e9ebcf687deec82
sechix <sechix@gmail.com>
2017-05-02 15:08:47 +0200

    aws s3 error

--- config/initializers/dragonfly.rb
+++ config/initializers/dragonfly.rb
@@ -11,25 +11,25 @@
 #
 # Pictures
 #
-Dragonfly.app(:alchemy_pictures).configure do
-  dragonfly_url nil
-  plugin :imagemagick
-  plugin :svg
-  secret '80ae5f828e02edae01729fa7a288b1be443209e0eac345e406cb74bd95d88550'
-  url_format '/pictures/:job/:name.:ext'
+# Dragonfly.app(:alchemy_pictures).configure do
+#   dragonfly_url nil
+#   plugin :imagemagick
+#   plugin :svg
+#   secret '80ae5f828e02edae01729fa7a288b1be443209e0eac345e406cb74bd95d88550'
+#   url_format '/pictures/:job/:name.:ext'
 
-  datastore :file,
-    root_path: Rails.root.join('uploads/pictures').to_s,
-    server_root: Rails.root.join('public'),
-    store_meta: false
-end
+#   datastore :file,
+#     root_path: Rails.root.join('uploads/pictures').to_s,
+#     server_root: Rails.root.join('public'),
+#     store_meta: false
+# end
 
-# Mount as middleware
-Rails.application.middleware.use Dragonfly::Middleware, :alchemy_pictures
+# # Mount as middleware
+# Rails.application.middleware.use Dragonfly::Middleware, :alchemy_pictures
 
-# Attachments
-Dragonfly.app(:alchemy_attachments).configure do
-  datastore :file,
-    root_path:  Rails.root.join('uploads/attachments').to_s,
-    store_meta: false
-end
+# # Attachments
+# Dragonfly.app(:alchemy_attachments).configure do
+#   datastore :file,
+#     root_path:  Rails.root.join('uploads/attachments').to_s,
+#     store_meta: false
+# end
