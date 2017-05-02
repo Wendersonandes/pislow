ac4ac921ebf961e248458c43435d4f8db856418f
sechix <sechix@gmail.com>
2017-05-02 22:33:27 +0200

    aws s3 error

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,17 +1,17 @@
-if Rails.env.production?
+Spree.config do |config|
   attachment_config = {
     s3_credentials: {
-      access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-      secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-      bucket: ENV['S3_BUCKET_NAME']
+      access_key_id:  ENV.fetch("AWS_ACCESS_KEY_ID"),
+      secret_access_key:  ENV.fetch("AWS_SECRET_ACCESS_KEY"),
+      bucket:  ENV.fetch("S3_BUCKET_NAME")
     },
 
     storage:        :s3,
     s3_headers:     { "Cache-Control" => "max-age=31557600" },
     s3_protocol:    "https",
-    s3_region:      ENV['AWS_REGION'],
-    s3_host_alias:  ENV['CDN_HOST'],
-    bucket:         ENV['S3_BUCKET_NAME'],
+    s3_region:       ENV.fetch("AWS_REGION"),
+    s3_host_alias:   ENV.fetch("CDN_HOST"),
+    bucket:          ENV.fetch("S3_BUCKET_NAME"),
 
     styles: {
             micro: '44x56>',
@@ -21,12 +21,13 @@ if Rails.env.production?
             large: '1422x1855>',
     },
 
-    path:          "/products/:id/:style/:basename.:extension",
+    path:          "/:class/:id/:style/:basename.:extension",
     default_url:   "noimage/:style.png",
     default_style: "product"
   }
 
-  attachment_config.each do |key, value|
+   attachment_config.each do |key, value|
     Spree::Image.attachment_definitions[:attachment][key.to_sym] = value
+    Spree::Taxon.attachment_definitions[:icon][key.to_sym] = value
   end
-end 
+end unless Rails.env.test?
